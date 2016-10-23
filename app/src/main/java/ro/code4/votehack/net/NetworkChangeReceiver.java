828ec5a6a1c2f0f26df2499c07239847e4edc475
Tudor Marze <tudor.marze@yahoo.com>
2016-10-23 14:32:42 +0200

    Add receiver for connectivity change

---
+++ app/src/main/java/ro/code4/votehack/net/NetworkChangeReceiver.java
@@ -0,0 +1,24 @@
+package ro.code4.votehack.net;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(isOnline(context)) {
+            //do stuff when connection received
+        } else {
+            //do stuff when connection lost
+        }
+    }
+
+    public boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnected()); //checking null for airplane mode
+    }
+}
