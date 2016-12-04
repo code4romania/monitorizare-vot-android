b2a0254b9650394fb09c74ac79ad53b8fe5beaee
Tudor Marze <tudor.marze@yahoo.com>
2016-12-04 14:17:49 +0100

    Add dummy guide

--- app/src/main/java/ro/code4/monitorizarevot/fragment/GuideFragment.java
+++ app/src/main/java/ro/code4/monitorizarevot/fragment/GuideFragment.java
@@ -1,11 +1,23 @@
 package ro.code4.monitorizarevot.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import ro.code4.monitorizarevot.BaseFragment;
 import ro.code4.monitorizarevot.R;
 
 public class GuideFragment extends BaseFragment {
     public static GuideFragment newInstance() {
         return new GuideFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_guide, container, false);
     }
 
     @Override
