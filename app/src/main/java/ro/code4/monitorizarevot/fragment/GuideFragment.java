e508eeca9608f5dca31f185c9727f0dc7a8a5568
MirelaL <tincuta.mirela.lalu@gmail.com>
2016-12-08 23:13:53 +0200

    - incarcare ghid utilizator

--- app/src/main/java/ro/code4/monitorizarevot/fragment/GuideFragment.java
+++ app/src/main/java/ro/code4/monitorizarevot/fragment/GuideFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import ro.code4.monitorizarevot.BaseFragment;
 import ro.code4.monitorizarevot.R;
@@ -18,6 +20,15 @@ public class GuideFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_guide, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        WebView guideView = (WebView)view.findViewById(R.id.web_view_guide);
+        WebSettings webSettings = guideView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        guideView.loadUrl("http://monitorizare-vot-ghid.azurewebsites.net/");
     }
 
     @Override
