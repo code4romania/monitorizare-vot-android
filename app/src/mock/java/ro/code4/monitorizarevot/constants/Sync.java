84f5eb4457502594e9bb7bb71038cb7ef5a8be0b
Alexandru Chiriac <a.chiriac@levi9.com>
2018-12-07 23:29:00 +0200

    Fix sync adapter auth

---
+++ app/src/mock/java/ro/code4/monitorizarevot/constants/Sync.java
@@ -0,0 +1,8 @@
+package ro.code4.monitorizarevot.constants;
+
+public class Sync {
+
+    public static final String AUTHORITY = "ro.code4.monitorizarevot.mock";
+
+    public static final String ACCOUNT_TYPE = "mock.code4.ro";
+}
