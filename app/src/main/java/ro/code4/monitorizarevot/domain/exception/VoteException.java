caab0f7e850e80c3e01d66e1276377ae0e8426d6
Tudor Marze <tudor.marze@yahoo.com>
2019-02-23 13:42:36 +0200

    Add no internet error on login and show custom error when received from server

--- app/src/main/java/ro/code4/monitorizarevot/domain/exception/VoteException.java
+++ app/src/main/java/ro/code4/monitorizarevot/domain/exception/VoteException.java
@@ -3,13 +3,27 @@ package ro.code4.monitorizarevot.domain.exception;
 public class VoteException extends Exception {
 
     private MessageType mType;
+    private String mCustomMessage;
 
     public VoteException(MessageType type) {
         super();
         mType = type;
     }
 
+    public VoteException(MessageType type, String customMessage) {
+        this(type);
+        mCustomMessage = customMessage;
+    }
+
     public MessageType getType() {
         return mType;
+    }
+
+    public String getCustomMessage() {
+        return mCustomMessage;
+    }
+
+    public boolean hasCustomMessage() {
+        return mCustomMessage != null && !mCustomMessage.trim().isEmpty();
     }
 }
