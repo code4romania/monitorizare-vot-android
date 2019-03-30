656b56a2821af0ed4e62fbac0b767e0ba93b84ca
Aurel_Chiper <aurel.chiper@safefleet.eu>
2019-03-30 15:22:20 +0200

    Add error message on invalid phone number

--- app/src/main/java/ro/code4/monitorizarevot/data/repository/AuthRepositoryImpl.java
+++ app/src/main/java/ro/code4/monitorizarevot/data/repository/AuthRepositoryImpl.java
@@ -1,10 +1,13 @@
 package ro.code4.monitorizarevot.data.repository;
 
+import android.telephony.PhoneNumberUtils;
+
 import static android.text.TextUtils.isEmpty;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import io.michaelrocks.libphonenumber.android.PhoneNumberUtil;
 import ro.code4.monitorizarevot.data.datasource.ApiDataSource;
 import ro.code4.monitorizarevot.data.datasource.DataSourceFactory;
 import ro.code4.monitorizarevot.domain.exception.MessageType;
@@ -28,6 +31,10 @@ public class AuthRepositoryImpl implements AuthRepository {
     public Observable<Boolean> login(LoginDataParams params) {
         if(params == null || isEmpty(params.getPhoneNumber()) || isEmpty(params.getPinNumber())) {
             return Observable.error(new VoteException(MessageType.EMPTY_CREDENTIALS));
+        }
+
+        if(!PhoneNumberUtils.isGlobalPhoneNumber(params.getPhoneNumber())) {
+            return Observable.error(new VoteException(MessageType.INVALID_PHONE_NUMBER));
         }
 
         ApiDataSource apiDataSource = mDataSourceFactory.dataSource(params.isLocal());
--- app/src/main/java/ro/code4/monitorizarevot/domain/exception/MessageType.java
+++ app/src/main/java/ro/code4/monitorizarevot/domain/exception/MessageType.java
@@ -2,6 +2,7 @@ package ro.code4.monitorizarevot.domain.exception;
 
 public enum MessageType {
     EMPTY_CREDENTIALS,
+    INVALID_PHONE_NUMBER,
     NO_INTERNET_CONNECTION,
     SERVER_ERROR,
     CUSTOM_ERROR,
